5d7834a5095a63cf2aed0c34437275aa0ed445c7
Ray <emanual0426@gmail.com>
2018-03-25 18:11:13 +0800

    add

--- Macro_demo/Macro_demo/Module1.vb
+++ Macro_demo/Macro_demo/Module1.vb
@@ -1,6 +1,22 @@
 ﻿Module Module1
+Imports SolidWorks.Interop.sldworks
+Imports SolidWorks.Interop.swconst
+Imports System
+Imports System.IO
 
     Sub Main()
+        Dim swDoc As ModelDoc2 = Nothing
+        Dim swPart As PartDoc = Nothing
+        Dim swDrawing As DrawingDoc = Nothing
+        Dim swAssembly As AssemblyDoc = Nothing
+        Dim boolstatus As Boolean = False
+        Dim longstatus As Integer = 0
+        Dim longwarnings As Integer = 0
+        Dim swApp As SldWorks.SldWorks
+        swApp = New SldWorks.SldWorks()
+        'When set "swApp.Visible" as "False", Solidworks won't pop out when running the macro
+        'When set "swApp.Visible" as "True", It is easier to debug when running the macro
+        swApp.Visible = True
 
     End Sub
 
